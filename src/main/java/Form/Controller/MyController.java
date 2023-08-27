3402b7225badb8c2928d9f19dba07e16aea94d28
Arifur Rahman <arif.bd08060@gmail.com>
2023-08-27 23:49:20 +0600

    imput data and show it in user menu

--- src/main/java/Form/Controller/MyController.java
+++ src/main/java/Form/Controller/MyController.java
@@ -1,14 +1,29 @@
 package Form.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import Form.Entity.LoginData;
 
 @Controller
 public class MyController {
 	@GetMapping("/form")
-	public String openForm() {
+	public String openForm(Model model) {
+		model.addAttribute("loginData", new LoginData());
 		return "form";
 		
+	}
+	
+	//handeller for post data
+	@PostMapping("/process")
+	//@ModelAttribute("loginData"): This annotation is used to bind the submitted form data to a Java object. 
+	public String processForm(@ModelAttribute("loginData") LoginData loginData) {
+		System.out.println(loginData);
+		
+		return "success";
 	}
 
 }
--- src/main/java/Form/Entity/LoginData.java
+++ src/main/java/Form/Entity/LoginData.java
@@ -1,5 +1,27 @@
 package Form.Entity;
 
 public class LoginData {
+	private String userName;
+	private String mailAddress;
+	
+	
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getMailAddress() {
+		return mailAddress;
+	}
+	public void setMailAddress(String mailAddress) {
+		this.mailAddress = mailAddress;
+	}
+	@Override
+	public String toString() {
+		return "LoginData [userName=" + userName + ", mailAddress=" + mailAddress + "]";
+	}
+	
+	
 
 }
